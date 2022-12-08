a60f4fff4158377dc684a77a2c45968f7caaf959
이선아 <iseon-a@iseon-aui-MacBookPro.local>
2022-12-09 06:41:26 +0900

    build router 테스트 코드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+
+import About from "./routes/admin/About";
+import Home from "./routes/Home";
+import Contact from "./routes/user/Contact";
+import Navigation from "./components/Navigation";
+
+import './scss/App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<Navigation />
+			<Route exact path="/" component={Home} />
+			<Route path="/about" component={About} />
+			<Route path="/contact" component={Contact} />
+		</BrowserRouter>
+	);
 }
 
 export default App;
